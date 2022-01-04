5524f0ac582029aa45704975e356db6a77a2c024
Todd Gillies <todbotts.triangles@gmail.com>
2022-01-04 17:39:05 +0900

    First commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace Example
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.chooseMP4 = new System.Windows.Forms.OpenFileDialog();
+            this.speechBox = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.setAudio = new System.Windows.Forms.OpenFileDialog();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtFileDialog
+            // 
+            this.txtFileDialog.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(21, 292);
+            this.button1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(222, 42);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Play";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(22, 342);
+            this.button2.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(222, 34);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Pause";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // chooseMP4
+            // 
+            this.chooseMP4.FileName = "openFileDialog1";
+            // 
+            // speechBox
+            // 
+            this.speechBox.Location = new System.Drawing.Point(252, 18);
+            this.speechBox.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.speechBox.Multiline = true;
+            this.speechBox.Name = "speechBox";
+            this.speechBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.speechBox.Size = new System.Drawing.Size(991, 606);
+            this.speechBox.TabIndex = 4;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(20, 522);
+            this.button3.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(220, 56);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Start";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(20, 125);
+            this.button4.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(220, 71);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "MP3 を選択";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // setAudio
+            // 
+            this.setAudio.FileName = "openFileDialog1";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(20, 586);
+            this.button5.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(222, 38);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Stop";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(17, 14);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(225, 107);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "字幕対象のビデオの音声を MP3 として保存し、下記のボタンで選択します。";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(20, 200);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(225, 88);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "それから、下記のボタンを使って、前に進んだり止めたりします。";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.Location = new System.Drawing.Point(20, 389);
+            this.label3.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(225, 129);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "音声が止まった状態で、下記のStart をクリックし、通訳を行い、Stop をクリックしてください。";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1268, 637);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.speechBox);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "Form1";
+            this.Text = "SRT Maker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.OpenFileDialog txtFileDialog;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.OpenFileDialog chooseMP4;
+        private System.Windows.Forms.TextBox speechBox;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.OpenFileDialog setAudio;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using WMPLib;
+using System.Speech.Recognition;
+using System.Linq;
+using NAudio.Wave;
+using System.IO;
+
+namespace Example
+{
+    public partial class Form1 : Form
+    {
+
+        public WindowsMediaPlayer myplayer = new WindowsMediaPlayer();
+        public string start = "00:00";
+        public string end = "00:00";
+        public int segment = 1;
+        public List<string> theseLines = new List<string>();
+        public bool running = false;
+
+
+
+
+
+
+        WaveIn sourceStream;
+        WaveFileWriter waveWriter;
+        public string FilePath;
+        public string FileName = "dictation.wav";
+
+
+
+        public void StartRecording(int index)
+        {
+            sourceStream = new WaveIn
+            {
+                DeviceNumber = index,
+                WaveFormat =
+                    new WaveFormat(44100, WaveIn.GetCapabilities(index).Channels)
+            };
+
+            sourceStream.DataAvailable += SourceStreamDataAvailable;
+
+            waveWriter = new WaveFileWriter(FilePath + "\\" + FileName, sourceStream.WaveFormat);
+            sourceStream.StartRecording();
+        }
+
+        public void SourceStreamDataAvailable(object sender, WaveInEventArgs e)
+        {
+            if (waveWriter == null) return;
+            waveWriter.Write(e.Buffer, 0, e.BytesRecorded);
+            waveWriter.Flush();
+        }
+
+        public void RecordEnd()
+        {
+            if (sourceStream != null)
+            {
+                sourceStream.StopRecording();
+                sourceStream.Dispose();
+                sourceStream = null;
+            }
+            if (waveWriter == null)
+            {
+                return;
+            }
+            waveWriter.Dispose();
+            waveWriter = null;
+        }
+
+
+
+
+
+
+
+        public Form1()
+        {
+            // This makes the window appear
+            InitializeComponent();
+
+        }
+
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            setAudio.Filter = "*.mp3|*.mp3|All files(*.*)|*.*";
+            if (setAudio.ShowDialog() == DialogResult.OK)
+            {
+                myplayer.URL = setAudio.FileName;
+                myplayer.controls.stop();
+                FilePath = Path.GetDirectoryName(setAudio.FileName);                
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            myplayer.controls.play();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            myplayer.controls.pause();
+
+            List<string> existingLines = speechBox.Lines.ToList();
+            foreach (string line in existingLines)
+            {
+                theseLines.Add(line);
+            }
+
+            theseLines.Add(segment.ToString());
+            theseLines.Add(start + " --> " + myplayer.controls.currentPositionString);
+            start = myplayer.controls.currentPositionString;
+            segment += 1;
+        }
+
+        private void recognizeNow(string fileName)
+        {
+                // Create an in-process speech recognizer for the en-US locale.  
+                using (
+                SpeechRecognitionEngine recognizer =
+                new SpeechRecognitionEngine(
+                new System.Globalization.CultureInfo("en-US")))
+                {
+
+                    // Create and load a dictation grammar.  
+                    recognizer.LoadGrammar(new DictationGrammar());
+
+                    // Add a handler for the speech recognized event.  
+                    recognizer.SpeechRecognized +=
+                        new EventHandler<SpeechRecognizedEventArgs>(recognizer_SpeechRecognized);
+
+                    // Configure input to the speech recognizer.  
+                    recognizer.SetInputToWaveFile(fileName);
+
+                    // Start speech recognition.
+                    recognizer.Recognize();
+                }
+
+            }        
+
+
+
+
+
+
+        // Handle the SpeechRecognized event.  
+        void recognizer_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            theseLines.Add(e.Result.Text);
+            theseLines.Add("");
+            speechBox.Lines = theseLines.ToArray();
+            theseLines.Clear();
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            StartRecording(0);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            RecordEnd();
+            recognizeNow(Path.Combine(FilePath, FileName));
+        }
+    }
+}
